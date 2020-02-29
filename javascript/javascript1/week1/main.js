bec608d1466cc71297a29a81425f9719c1c79245
deepthi <deepthidukka@gmail.com>
2020-02-29 08:16:22 +0100

    implemented 1.Future age calculator

---
+++ javascript/javascript1/week1/main.js
@@ -0,0 +1,5 @@
+/*1. Future age calculator*/
+let yearOfBirth = 1989;
+let yearFuture = 2045;
+let age = yearFuture - yearOfBirth;
+console.log('Your age will be either ' + age + ' or ' + (age - 1));

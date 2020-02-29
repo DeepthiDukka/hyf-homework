e8325412925a528688960e989014df8468d69479
deepthi <deepthidukka@gmail.com>
2020-02-29 08:22:23 +0100

    implemented 2. Dog age calculator

--- javascript/javascript1/week1/main.js
+++ javascript/javascript1/week1/main.js
@@ -3,3 +3,18 @@ let yearOfBirth = 1989;
 let yearFuture = 2045;
 let age = yearFuture - yearOfBirth;
 console.log('Your age will be either ' + age + ' or ' + (age - 1));
+
+/*2. Dog age calculator*/
+
+let dogYearOfBirth = 2019;
+let dogYearFuture = 2027;
+let humanYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = 7 * humanYear;
+
+if (shouldShowResultsInDogYears = true) {
+        console.log(`Your dog age will be ${shouldShowResultInDogYears} dog years old in 2027.`);
+
+} else {
+        console.log(`Your dog age will be + ${ humanYear} + human years old in 2027.`);
+
+}

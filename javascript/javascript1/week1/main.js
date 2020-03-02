3917df83ccc3b9e833a1f92c934351435550c2d1
deepthi <deepthidukka@gmail.com>
2020-03-02 23:58:13 +0100

    Implemented feedback on age and dog year exercises

--- javascript/javascript1/week1/main.js
+++ javascript/javascript1/week1/main.js
@@ -2,20 +2,21 @@
 let yearOfBirth = 1989;
 let yearFuture = 2045;
 let age = yearFuture - yearOfBirth;
-console.log('Your age will be either ' + age + ' or ' + (age - 1));
+console.log('Your age will be' + age);
 
 /*2. Dog age calculator*/
 
 let dogYearOfBirth = 2019;
 let dogYearFuture = 2027;
 let humanYear = dogYearFuture - dogYearOfBirth;
-let shouldShowResultInDogYears = 7 * humanYear;
-let shouldShowResultInDogYears = true;
-if (shouldShowResultsInDogYears) {
-        console.log(`Your dog age will be ${shouldShowResultInDogYears} dog years old in 2027.`);
+let dogYear = 7 * humanYear;
+let shouldShowResultInDogYears = dogYear;
+
+if (shouldShowResultsInDogYears=true) {
+        console.log(`Your dog age will be ${dogYear} dog years old in 2027.`);
 
 } else {
-        console.log(`Your dog age will be + ${ humanYear} + human years old in 2027.`);
+        console.log(`Your dog age will be ${ humanYear} human years old in 2027.`);
 
 }
 

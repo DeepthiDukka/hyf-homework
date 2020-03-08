5826f0a624c547ed4a24fca90e27e42018fca6dd
deepthi <deepthidukka@gmail.com>
2020-03-08 14:21:01 +0100

    changes made on if conditions

--- javascript/javascript1/week2/mainweek2.js
+++ javascript/javascript1/week2/mainweek2.js
@@ -12,7 +12,7 @@ function getFullname(firstname, surname, useFormalName) {
     }
 
 }
-const fullname1 = getFullname("Deepthi", "" );
+const fullname1 = getFullname("Deepthi", "");
 const fullname2 = getFullname("Supriya", "Dukka", true);
 
 console.log(fullname1);
@@ -35,7 +35,7 @@ console.log(getEventWeekday(9));
 /* Weather wear */
 
 function weatherWear(temperature) {
-   
+
     if (temperature >= 0 && temperature <= 4) {
         return "Parka Jakke , Sweater and woolen trouser";
     } else if (temperature >= 4 && temperature <= 8) {
@@ -52,57 +52,57 @@ weatherWear(3);
 
 /*Student manager*/
 let class07Students = [];
+
 function addStudentToClass(studentName) {
-    if (class07Students.length >= 6 && studentName !== "Queen"){
-        alert ("Cannot add more students to class 07");
-    } 
-    
-    else if (class07Students.includes(studentName)) {
-        alert ("Student " + studentName + " is already in the class");
+    if (class07Students.length >= 6 && studentName !== "Queen") {
+        alert("Cannot add more students to class 07");
+    } else if (class07Students.includes(studentName)) {
+        alert("Student " + studentName + " is already in the class");
+    } else if (!studentName.trim()) {
+        alert("You cannot add an empty string to class 07");
+    } else {
+        class07Students.push(studentName);
     }
-else if (!studentName.trim()){
-    alert ("You cannot add an empty string to class 07");
 }
-else {
-    class07Students.push(studentName);
-}
-}
+
 function getNumberOfStudents() {
     console.log(class07Students);
     console.log(getNumberOfStudents());
     return class07Students.length;
-    
+
 }
 
 /*addCandy function*/
 let boughtCandyPrices = [];
 let amountToSpend = Math.random() * 100;
+
 function canBuyMoreCandy() {
-    let amountSpent = 0, i = 0;
-    while(i < boughtCandyPrices.length){
+    let amountSpent = 0,
+        i = 0;
+    while (i < boughtCandyPrices.length) {
         amountSpent += boughtCandyPrices[i];
         i++;
     }
-    if(amountToSpend > amountSpent){
-       return "You can buy more, so please do!";
+    if (amountToSpend > amountSpent) {
+        return "You can buy more, so please do!";
     } else {
-       return "Enough candy for you!";
+        return "Enough candy for you!";
     }
 }
 
-function addCandy(candyType,weight) {
+function addCandy(candyType, weight) {
     let calculatedPrice;
- if(candyType === "sweet"){
-    calculatedPrice = weight * 0.5;
-} else if(candyType === "chocolate"){
-    calculatedPrice = weight * 0.7;
-} else if(candyType === "toffee"){
-    calculatedPrice = weight * 1.1;
-} else if(candyType === "chewing-gum"){
-    calculatedPrice = weight * 0.3;
-}
-boughtCandyPrices.push(calculatedPrice);
-return canBuyMoreCandy();
+    if (candyType === "sweet") {
+        calculatedPrice = weight * 0.5;
+    } else if (candyType === "chocolate") {
+        calculatedPrice = weight * 0.7;
+    } else if (candyType === "toffee") {
+        calculatedPrice = weight * 1.1;
+    } else if (candyType === "chewing-gum") {
+        calculatedPrice = weight * 0.3;
+    }
+    boughtCandyPrices.push(calculatedPrice);
+    return canBuyMoreCandy();
 }
 
 addCandy("sweet", 20);

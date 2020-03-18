5f106876ff094315f0f1297b22a38ecea839ce00
deepthi <deepthidukka@gmail.com>
2020-03-18 05:51:23 +0100

    Implemented feedback to get return obj form but unable to get output

--- javascript/javascript1/week3/travelTime.js
+++ javascript/javascript1/week3/travelTime.js
@@ -10,7 +10,20 @@ function timeTakenForDestination(travelInformation) {
   let totalHours = Math.floor(time);
   let totalMinutes = Math.round((time - totalHours) * 60);
   return `${totalHours} hours and ${totalMinutes} minutes`
-}
-
+  };
 const travelTime = timeTakenForDestination(travelInformation);
 console.log("Total travel time is : " + travelTime);
+
+//As per the feedback: implemented return object but unable to get the output
+function timeCalculation(){
+  const time = distance / speed;
+  let obj = {
+    totalHours: Math.floor(time),
+    totalMinutes: Math.round((time - totalHours)*60)
+  };
+  return obj;
+};
+const speed = 50;
+const distance = 432;
+const travelTime1 = timeCalculation();
+console.log(travelTime1);

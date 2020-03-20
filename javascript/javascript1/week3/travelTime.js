74f241386519062a9e317a1ac55315f8899ecc7c
deepthi <deepthidukka@gmail.com>
2020-03-20 07:25:07 +0100

    Implemented feedback on return object format

--- javascript/javascript1/week3/travelTime.js
+++ javascript/javascript1/week3/travelTime.js
@@ -10,20 +10,26 @@ function timeTakenForDestination(travelInformation) {
   let totalHours = Math.floor(time);
   let totalMinutes = Math.round((time - totalHours) * 60);
   return `${totalHours} hours and ${totalMinutes} minutes`
-  };
+};
 const travelTime = timeTakenForDestination(travelInformation);
 console.log("Total travel time is : " + travelTime);
 
-//As per the feedback: implemented return object but unable to get the output
-function timeCalculation(){
-  const time = distance / speed;
+//As per the feedback: implemented return object 
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function timeTakenForDestination(travelInformation) {
+
+  let time = travelInformation.destinationDistance / travelInformation.speed;
+  let totalHours = Math.floor(time);
+  let totalMinutes = Math.round((time - totalHours) * 60);
   let obj = {
-    totalHours: Math.floor(time),
-    totalMinutes: Math.round((time - totalHours)*60)
+    totalHours: totalHours,
+    totalMinutes: totalMinutes
   };
   return obj;
-}
-const speed = 50;
-const distance = 432;
-const travelTime1 = timeCalculation();
-console.log(travelTime1);
+};
+const travelTime = timeTakenForDestination(travelInformation);
+console.log("Total travel time is : " + travelTime);

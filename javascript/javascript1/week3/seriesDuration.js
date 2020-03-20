c9f9ab52f4ec64bb94f21c0b2a032c9199d0c217
deepthi <deepthidukka@gmail.com>
2020-03-20 20:22:10 +0100

    Implemented feedback : Series Duration code using function

--- javascript/javascript1/week3/seriesDuration.js
+++ javascript/javascript1/week3/seriesDuration.js
@@ -36,3 +36,48 @@ for (let i = 0; i < seriesDurations.length; i++) {
 
 }
 console.log(`In total that is ${total}% of my life`);
+
+//As per the given feedback : Series Duration code using functions:
+
+const seriesDurations1 = [{
+    title: 'Game of thrones',
+    days: 3,
+    hours: 1,
+    minutes: 0,
+},
+{
+    title: 'Quantico',
+    days: 1,
+    hours: 16,
+    minutes: 51,
+},
+{
+    title: 'Altered Carbon',
+    days: 6,
+    hours: 1,
+    minutes: 4,
+}
+];
+
+function calculatePercentage(years) {
+    return Number(years * 100 / 80).toFixed(4);
+}
+
+function calculateYears(days,hours,minutes){
+    const newHours = hours + (minutes/60);
+    const newDays = days + (newHours/24);
+    const years = (newDays/365);
+    return years;
+}
+
+function calculateTimeTakenInMyLife(seriesTime) {
+    let totalTime = 0;
+    for (let i = 0;i < seriesTime.length; i++){
+        const time = calculateYears(seriesTime[i].days,seriesTime[i].hours,seriesTime[i].minutes);
+        totalTime += time;
+        console.log(seriesTime[i].title + " took " + calculatePercentage(time) + "% of my life");
+    }
+    return totalTime;
+}
+const totalTime = calculateTimeTakenInMyLife(seriesDurations1);
+console.log("In total that is " + calculatePercentage(totalTime) + "% of my life");

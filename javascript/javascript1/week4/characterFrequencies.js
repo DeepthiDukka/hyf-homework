9c9cfd32eccff71a57db646b5132a14b74a77d03
deepthi <deepthidukka@gmail.com>
2020-04-01 05:21:54 +0200

    implemented feedback by using loop

--- javascript/javascript1/week4/characterFrequencies.js
+++ javascript/javascript1/week4/characterFrequencies.js
@@ -1,3 +1,27 @@
+// method using loops
+const string = "happy";
+
+function getFrequency(string) {
+    let freq = {};
+    for (let i = 0; i < string.length; i++) {
+        let character = string.charAt(i);
+        if (freq[character]) {
+            freq[character]++;
+        } else {
+            freq[character] = 1;
+
+        }
+    }
+
+    return freq;
+
+}
+console.log(getFrequency(string));
+console.log('length of the string:', str, 'is', str.length);
+
+
+
+// other method
 const str = 'happy';
 
 function getCharacterFrequencies(target) {

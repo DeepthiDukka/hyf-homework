1f7662410f04fc4b8169ee8b216edbff3076c2f4
deepthi <deepthidukka@gmail.com>
2020-04-03 01:26:27 +0200

    revised code as per feedback

--- javascript/javascript1/week4/palindromeSubstring.js
+++ javascript/javascript1/week4/palindromeSubstring.js
@@ -1,4 +1,38 @@
 //A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward. eg: madam
+
+function longestPalindrome1(str) {
+  const entireString = str.toLowerCase();
+  if (str.length < 2) {
+    return "";
+  }
+
+  let palindrome = "";
+
+  for (let i = 0; i < entireString.length; i++) {
+    for (let j = 1; j <= entireString.length - i; j++) {
+      if (entireString.substr(i, j) === stringReverser(entireString, i, j)) { //check if string[i,j] is palindrom
+        if (j > palindrome.length) {                 //if it's true then check if the size of string[i,j] is higher then the current value and store it
+          palindrome = entireString.substr(i, j);  
+        }
+      }
+    }
+  }
+
+  if (!palindrome) {
+    return null;
+  }
+
+  else return palindrome;
+}
+function stringReverser(stringToReverse, start, length) {
+  const reversedWord = stringToReverse.substr(start, length).split('').reverse().join(''); // as reverse won't work directly so using split and join.
+  return reversedWord;
+}
+
+let result1 = longestPalindrome1("nan noon is redder malayalam");
+console.log("The longest palindromic substring of a given string is: " + result1 + " and length is: " + result1.length);
+
+/*
 let longestPalindrome = function(string) {
 
     let length = string.length;
@@ -34,4 +68,4 @@ let longestPalindrome = function(string) {
   
   console.log(
     longestPalindrome("nan noon is redder malayalam")
-  );
+  );*/

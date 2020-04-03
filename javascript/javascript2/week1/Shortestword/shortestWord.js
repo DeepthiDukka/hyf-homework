a91d2c2d36b53389d7bb1829eb423b95ab31c653
deepthi <deepthidukka@gmail.com>
2020-04-04 01:47:00 +0200

    implemented feedback as suggested

--- javascript/javascript2/week1/Shortestword/shortestWord.js
+++ javascript/javascript2/week1/Shortestword/shortestWord.js
@@ -1,14 +1,15 @@
 // Method 1 of finding the shortest word in an array
-const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium',[], 0, null, undefined, 'aa', Infinity, true];
+const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium', [], 0, null, undefined, 'aa', Infinity, true];
+
 function findShortestWord(word) {
-let shortest = word[0];
-for(let i=0; i < word.length; i++){
+  let shortest = word[0];
+  for (let i = 0; i < word.length; i++) {
     if (typeof word[i] === 'string' &&
-        (shortest === ''|| word[i].length < shortest.length)) {
+      (word[i].length < shortest.length)) {
       shortest = word[i];
     }
   }
-  
+
   return shortest;
 };
 let result = findShortestWord(danishWords);
@@ -17,15 +18,15 @@ console.log(result);
 
 // Method 2 of finding the shortest word in a array
 function shortestWord(arr) {
-    let shortest = '';
-    // function to check the array elements length
-    arr.map(function(str){ 
-        if (str.length < shortest.length){
-            shortest = str;
-        }
-    })
-    return shortest;
+  let shortest = '';
+  // function to check the array elements length
+  arr.forEach(function (str) {
+    if (str.length < shortest.length) {
+      shortest = str;
+    }
+  })
+  return shortest;
 }
 
-const shortestWordInArray = arr => arr.reduce((base,element) => element.length < base.length ? element : base);
+const shortestWordInArray = arr => arr.reduce((base, element) => element.length < base.length ? element : base);
 console.log(shortestWordInArray(['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium', 'my']));
